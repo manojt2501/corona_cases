5e59f01d183c988e7ef8aacc18eb7d9e4646a169
manojt2501 <manojt2501@gmail.com>
2021-03-15 23:48:18 +0530

    modified content in print statement

--- request.py
+++ request.py
@@ -2,8 +2,8 @@ import requests
 def get_content(url):
     response = requests.get(url)
     if response.status_code == 200:
-        print(f"Response successful: {response.status_code}")
+        print(f"Response successful. response code: {response.status_code}")
         return response.json()
     else:
-        print(f"Request completed with Error. Response Code : {response.status_code}")
+        print(f"Error with request. Response Code : {response.status_code}")
         return None
